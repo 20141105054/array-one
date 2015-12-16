9fed3a46711ca1f77abf83910357b040ea0f2fb5
songzhiyuan <www.1319324030@qq.com>
2015-12-16 19:06:47 +0800

    Initial Commit

---
+++ array one/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  array one
+//
+//  Created by 20141105054 on 15/12/16.
+//  Copyright © 2015年 宋志远. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
